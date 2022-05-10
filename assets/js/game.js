708d733d0604830826e2eb2ea8ef53ea096f2fd0
Strider <dave.barnes23@gmail.com>
2022-05-10 10:30:44 -0500

    adding additional enemy robots and having each be attacked/attack

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,12 +6,19 @@ var playerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
+// console.log(enemyNames);
+// console.log(enemyNames[0]);
+// console.log(enemyNames[1]);
+// console.log(enemyNames[2]);
+// console.log(enemyNames.length);
+
+
 
 // fight function
-var fight = function() {
+var fight = function(enemyName) {
     // Alert players we are starting the round
     window.alert("Welcome to robot Gladiators!");
 
@@ -70,4 +77,13 @@ var fight = function() {
     }
 };
 
-fight ();
+for(var i=0; i<enemyNames.length; i++ ) {
+    fight(enemyNames[i]);
+    // console.log(enemyNames[i]);
+    // console.log(i);
+    // console.log(enemyNames[i] + " is at " + i + " index");
+}
+
+// var fight = function(enemyName) {
+//     // fight function statements.
+// }
